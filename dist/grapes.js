40e0203aa9620d866d122b6aeb7e8014d8af064a
Miao (Wilson) Liu <wilson@clicktocloud.com>
2018-11-05 15:10:41 +1100

    update build script

--- dist/grapes.js
+++ dist/grapes.js
@@ -38089,7 +38089,7 @@ module.exports = function () {
     plugins: plugins,
 
     // Will be replaced on build
-    version: '0.14.43',
+    version: '0.14.44',
 
     /**
      * Initialize the editor with passed options
